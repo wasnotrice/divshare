ee976444f02aa170413ca0f66e135f71583c4854
Eric Watson <eric@brownline.net>
2007-11-15 14:11:48 -0600

    Basic file-type recognition & specs

--- spec/divshare_file_spec.rb
+++ spec/divshare_file_spec.rb
@@ -51,6 +51,49 @@ describe "An audio DivshareFile" do
   end
 end
 
+describe "An audio DivshareFile" do
+  include DivshareFileSpecHelper
+  before(:each) do
+    @xml = Hpricot(get_one_file_xml)/:file
+    @file_name = "audio.mp3"
+    @xml.at(:file_name).swap("<file_name>#{@file_name}</file_name>")
+    @file = DivshareFile.new(@xml)
+  end
+  it_should_behave_like "A basic DivshareFile"
+  
+  it "should know it is audio" do
+    @file.should be_audio
+  end
+  
+  it "should not think it is video, image, or document" do
+    @file.should_not be_video
+    @file.should_not be_image
+    @file.should_not be_document
+  end
+end
+
+describe "A video DivshareFile" do
+  include DivshareFileSpecHelper
+  before(:each) do
+    @xml = Hpricot(get_one_file_xml)/:file
+    @file_name = "video.mov"
+    @xml.at(:file_name).swap("<file_name>#{@file_name}</file_name>")
+    @file = DivshareFile.new(@xml)
+  end
+  it_should_behave_like "A basic DivshareFile"
+  
+  it "should know it is video" do
+    @file.should be_video
+  end
+  
+  it "should not think it is audio, image, or document" do
+    @file.should_not be_audio
+    @file.should_not be_image
+    @file.should_not be_document
+  end
+end
+
+
 describe "A document DivshareFile" do
   include DivshareFileSpecHelper
   before(:each) do
