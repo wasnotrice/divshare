1c3c6aa14bfd9e5843389efbe650109339ece9a1
Eric Watson <eric@brownline.net>
2007-11-15 14:10:57 -0600

    Basic file-type recognition & specs

--- lib/divshare/divshare_file.rb
+++ lib/divshare/divshare_file.rb
@@ -4,6 +4,11 @@ require 'hpricot'
 module Divshare
   class DivshareFile
     ATTRIBUTES = %w(file_id file_name file_description file_size downloads last_downloaded_at uploaded_at folder_title folder_id)
+    AUDIO = /^\.(mp3)$/i
+    VIDEO = /^\.(avi|wmv|mov|mpg|asf)$/i
+    DOCUMENT = /^\.(doc|pdf|ppt)$/i
+    IMAGE = /^\.(jpg|gif|png)/i
+    
     attr_accessor *ATTRIBUTES
     
     def initialize(xml)
@@ -11,6 +16,33 @@ module Divshare
         value = xml.at(attr).inner_html
         instance_variable_set("@#{attr}", value)
       end
+    end
+    
+    def medium
+      ext = File.extname(file_name)
+      medium = case
+        when AUDIO.match(ext): :audio
+        when VIDEO.match(ext): :video
+        when DOCUMENT.match(ext): :document
+        when IMAGE.match(ext): :image
+        else nil
+      end
+    end
+    
+    def audio?
+      medium == :audio
+    end
+    
+    def document?
+      medium == :document
+    end
+    
+    def video?
+      medium == :video
+    end
+    
+    def image?
+      medium == :image
     end
     
     def to_s
---
+++ spec/divshare_file_spec.rb
@@ -0,0 +1,72 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require 'divshare/divshare_file'
+include Divshare
+
+module DivshareFileSpecHelper
+  include DivshareMockXML
+end
+
+describe "A DivshareFile created with an empty file description" do
+  include DivshareFileSpecHelper
+  before(:each) do
+    @xml = Hpricot(get_one_file_xml)/:file
+    @xml_with_blank = @xml.dup
+    (@xml_with_blank.at :file_description).swap("<file_description></file_description>")
+    @file = DivshareFile.new(@xml_with_blank)
+  end
+
+  it "should treat an empty attribute in creation xml as an empty string" do
+    @file.file_description.should == ""
+  end
+
+end
+
+describe "A basic DivshareFile", :shared => true do
+  include DivshareFileSpecHelper
+
+  it "should set instance variables at creation" do
+    @file.instance_variables.map {|i| @file.instance_variable_get(i) }.length.should == 9
+  end
+    
+end
+
+describe "An audio DivshareFile" do
+  include DivshareFileSpecHelper
+  before(:each) do
+    @xml = Hpricot(get_one_file_xml)/:file
+    @file_name = "audio.mp3"
+    @xml.at(:file_name).swap("<file_name>#{@file_name}</file_name>")
+    @file = DivshareFile.new(@xml)
+  end
+  it_should_behave_like "A basic DivshareFile"
+  
+  it "should know it is audio" do
+    @file.should be_audio
+  end
+  
+  it "should not think it is video, image, or document" do
+    @file.should_not be_video
+    @file.should_not be_image
+    @file.should_not be_document
+  end
+end
+
+describe "A document DivshareFile" do
+  include DivshareFileSpecHelper
+  before(:each) do
+    @xml = Hpricot(get_one_file_xml)/:file
+    @file = DivshareFile.new(@xml)
+  end
+  it_should_behave_like "A basic DivshareFile"
+  
+  it "should know it is a document" do
+    @file.should be_document
+  end
+  
+  it "should not think it is video, image, or audio" do
+    @file.should_not be_video
+    @file.should_not be_image
+    @file.should_not be_audio
+  end
+  
+end
